f12827d7232caa5009a63380c5f71b1ed5a09736
cpsa3 <1090303279@qq.com>
2015-10-30 17:02:05 +0800

    移除无关引用

--- MvcSeed.Component.WeiXin/Helpers/WeiXinUtility.cs
+++ MvcSeed.Component.WeiXin/Helpers/WeiXinUtility.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MvcSeed.Component.Util;
+﻿using MvcSeed.Component.Util;
+using System;
 
 namespace MvcSeed.Component.WeiXin.Helpers
 {
---
+++ MvcSeed.Repository/Repo/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvcSeed.Component.Data;
+using MvcSeed.Repository.Entity;
+
+namespace MvcSeed.Repository.Repo
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(IUnitOfWork uw) : base(uw) { }
+    }
+}

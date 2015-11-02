34b4cec0fe69a920f3bf74fceec81dab40791b60
cpsa3 <1090303279@qq.com>
2015-11-02 20:52:28 +0800

    添加BCrypt加密方法

---
+++ MvcSeed.Component/Helpers/HashHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcSeed.Component.Helpers
+{
+    public class HashHelper
+    {
+        public static string BCryptPassword(string password, int workFactor = 12)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, workFactor);
+        }
+
+        public static bool BCryptVerify(string password, string hashed)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashed);
+        }
+    }
+}

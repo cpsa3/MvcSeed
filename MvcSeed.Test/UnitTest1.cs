cf6eff92dea271b5337970f68c40f5f13c45b58a
cpsa3 <1090303279@qq.com>
2015-11-02 21:13:43 +0800

    添加单元测试

--- MvcSeed.Test/UnitTest1.cs
+++ MvcSeed.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcSeed.Component.Helpers;
 
 namespace MvcSeed.Test
 {
@@ -7,8 +8,24 @@ namespace MvcSeed.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BCryptTest()
         {
+            const string password = "PASSWORD";
+            const int workFactor = 12;
+
+            var start = DateTime.UtcNow;
+            var hashed = HashHelper.BCryptPassword(password, workFactor);
+            var end = DateTime.UtcNow;
+            var hashed2 = HashHelper.BCryptPassword(password, workFactor);
+
+            var elapsedTime = end - start;
+
+            Assert.IsTrue(HashHelper.BCryptVerify("PASSWORD", hashed));
+            Assert.IsFalse(HashHelper.BCryptVerify("PASSWORd", hashed));
+            Assert.IsTrue(HashHelper.BCryptVerify("PASSWORD", hashed2));
+            Assert.IsFalse(HashHelper.BCryptVerify("PASSWORd", hashed2));
+            Assert.AreEqual(hashed.Length, 60);
+            Assert.AreNotEqual(hashed, hashed2);
         }
     }
 }
